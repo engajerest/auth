f08638dcbd0124f675b7d623951e8eb80d5d18df
engajerest <engaje.cloud@gmail.com>
2021-01-23 18:47:59 -0800

    last commit

--- calc/calc.go
+++ calc/calc.go
@@ -1,5 +1,5 @@
 package calc
 
-func addnum(i int, j int) int {
+func Addnum(i int, j int) int {
 	return i + j
 }
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/engajerest/auth/calc"
 )
 
 func main() {
-
+calc.Addnum(5,5)
 	fmt.Println("hello")
 
 }
